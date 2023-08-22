fc86c634318b7dc9aaf4db9ec3e85e09fa7d0e49
Mirella <mirellatcg@gmail.com>
2023-08-22 15:20:16 -0300

    up

--- Classes.py
+++ Classes.py
@@ -1,18 +1,18 @@
 class Aves:
     def botarOvo(self):
-        print ("É um animal ovíparo")
+        print ("É um animal ovíparo.")
 
     def pena(self):
-        print ("Possui penas")
+        print ("Essa ave possui penas")
 
     def bico(self):
-        print ("Possui um bico, não possui dentes")
+        print ("Essa ave possui um bico, não possui dentes")
 
 class Voar (Aves):
     def voar(self):
-        print ("Possui asas e é capaz de voar")
+        print ("Essa ave possui asas e é capaz de voar")
 
 class NãoVoar (Aves):
     def nãovoar(self):
-        print ("Não é capaz de voar")
+        print ("Essa ave ão é capaz de voar")
 
